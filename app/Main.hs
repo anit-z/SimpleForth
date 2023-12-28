8e88277511ce19ea1ca4759362b3e6374d4ceaba
anit-z <will_zh@163.com>
2023-12-28 20:10:31 +0800

    Finish main

--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,27 @@ import Interpreter
 import Parser
 import System.IO (hFlush, stdout)
 
+-- loop until quit
 loop :: Stack -> IO ()
 loop st = do
+  -- show prompt
   putStr "> "
   hFlush stdout
+  -- get input line
   str <- getLine
+  -- parse commands
   let cmds = parse str
+  -- interpret
   res <- interpret st cmds
   case res of
+    -- fail, quit
     Left err -> do
       print err
       return ()
+    -- success, loop use new stack
     Right st' -> loop st'
 
+-- main program
 main :: IO ()
+-- use loop
 main = loop []
